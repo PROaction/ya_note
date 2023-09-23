b36ba671d4e4393ca24a314609a3ffec522b0cfc
Сергей Попов <sergey1404sergey@mail.ru>
2023-09-23 16:22:51 +0300

    add routs tests.

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -15,24 +15,84 @@ class TestRoutes(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Автор заметки')
+        cls.other_user = User.objects.create(username='Просто пользователь')
         cls.note = Note.objects.create(
             title='Заметка',
             text='Текст заметки',
             author=cls.author,
         )
 
-    def test_pages_available_for_anonymous(self):
-        urls = (
+    def check_redirect(self, response, url):
+        login_url = reverse('users:login')
+        redirect_url = f'{login_url}?next={url}'
+        self.assertRedirects(response, redirect_url)
+
+    def test_available_pages_for_anonymous(self):
+        available_urls = (
             ('notes:home', None),
-            ('login', None),
-            ('logout', None),
-            ('signup', None),
-            ('notes:detail', (self.note.id, )),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
         )
 
-        for template_name, args in urls:
-            print('template_name:', template_name)
-            with self.subTest(name=template_name):
-                url = reverse(template_name, args=args)
+        login_only_urls = (
+            ('notes:add', None),
+            ('notes:edit', (self.note.slug,)),
+            ('notes:delete', (self.note.slug,)),
+            ('notes:detail', (self.note.slug,)),
+            ('notes:list', None),
+            ('notes:success', None),
+        )
+
+        status_check = (
+            (available_urls, HTTPStatus.OK),
+            (login_only_urls, HTTPStatus.FOUND),
+        )
+
+        for urls, expected_status_code in status_check:
+            for name, args in urls:
+                url = reverse(name, args=args)
                 response = self.client.get(url)
-                self.assertEqual(response.status_code, HTTPStatus.OK)
+                actual_status_code = response.status_code
+                self.assertEqual(actual_status_code, expected_status_code)
+                if actual_status_code == HTTPStatus.FOUND:
+                    self.check_redirect(response, url)
+
+    def test_personal_CRUD_for_notes(self):
+        users_status = (
+            (self.author, HTTPStatus.OK),
+            (self.other_user, HTTPStatus.NOT_FOUND),
+        )
+        personal_urls = (
+            ('notes:edit', (self.note.slug,)),
+            ('notes:delete', (self.note.slug,)),
+            ('notes:detail', (self.note.slug,)),
+        )
+        for user, status in users_status:
+            self.client.force_login(user)
+            for name, args in personal_urls:
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=args)
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+
+    #
+    #     for name, args in available_urls:
+    #         print('template_name:', name)
+    #         with self.subTest(name=name):
+    #             url = reverse(name, args=args)
+    #             response = self.client.get(url)
+    #             self.assertEqual(response.status_code, HTTPStatus.OK)
+    #
+    # def test_available_pages_for_anonymous(self):
+    #     urls = (
+    #         ('notes:add', None),
+    #         ('notes:edit', (self.note.slug, )),
+    #         ('notes:delete', (self.note.slug, )),
+    #         ('notes:detail', (self.note.slug,)),
+    #         ('notes:list', None),
+    #     )
+    #
+    #     for name, args in urls:
+
